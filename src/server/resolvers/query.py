6ad0164bcf7a239512dbfb06081941910e081181
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-02 07:17:10 +0900

    scaffolding(BE): media, search query resolver 구현

--- src/server/resolvers/query.py
+++ src/server/resolvers/query.py
@@ -1,11 +1,30 @@
-from ariadne import ObjectType
+from ariadne import ObjectType, convert_kwargs_to_snake_case
+from mock_data import DB
 
 query = ObjectType("Query")
 
 
 @query.field("media")
 def resolve_media(_, info, _id):
-    db: dict = info.context["db"]
-    videos: dict = db["videos"]
+    videos: dict = DB["videos"]
 
-    return videos[_id]
+    return videos[int(_id)]
+
+
+@convert_kwargs_to_snake_case
+@query.field("search")
+def resolve_search(
+    _,
+    info,
+    title: str = "",
+    location: str = "",
+    date_from="1900-01-01",
+    date_to="2099-12-31",
+):
+    videos: dict = DB["videos"]
+    videos = filter(lambda video: video["title"].find(title) != -1, videos)
+    videos = filter(lambda video: video["location"].find(location) != -1, videos)
+
+    # TODO(yun-kwak): 날짜 기반 필터링 구현
+
+    return videos
