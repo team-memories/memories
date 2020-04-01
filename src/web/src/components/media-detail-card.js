2468cefa00683d34aefd0fa423914a21fc8d3b2c
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-04-01 17:14:40 +0900

    feat: Implement media detail card delete button
    Implement media detail card delete button
    Add todo and comment

--- src/web/src/components/media-detail-card.js
+++ src/web/src/components/media-detail-card.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from "prop-types";
 import {Col, List, Avatar, Button, Skeleton} from 'antd';
+import MediaDetailDeleteButton from './media-detail-delete-button'
 
+// TODO(Lhyejin): Change fake data to media information props received  MediaDetailQuery
 class MediaDetailCard extends Component {
   // MediaDetailCard constructor
   constructor(props) {
     super(props);
+    // fake data
     this.state = {
+          id : 1,
           avatar : "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
           user : "이혜진",
           title : 'title',
@@ -15,12 +19,13 @@ class MediaDetailCard extends Component {
           videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
         }
   }
-  // Video Information Component
-  VideoInfo = () => {
+
+  // Media Information Component
+  MediaInfo = () => {
     return(
       <List>
         <List.Item
-          actions={[<Button>delete</Button>]}
+          actions={[<MediaDetailDeleteButton mediaId={this.state.id} />]}
         >
           <List.Item.Meta
             avatar={<Avatar size={50} src={this.state.avatar}/>}
@@ -34,14 +39,14 @@ class MediaDetailCard extends Component {
     )
   }
 
-  // Render of Media
+  // Render of Media Detail Card
   render() {
     return (
       <div>
           <Col xs={24}>
             <div style={{width: '80%', padding: '3rem 4rem'}}>
               <video style={{width: '100%', height: '80%'}} src={this.state.videoUrl} controls />
-              <this.VideoInfo />
+              <this.MediaInfo />
             </div>
           </Col>
       </div>
