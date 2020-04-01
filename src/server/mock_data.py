af35ee52bf63b45b1396ed4baea62aad9016b3b9
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-02 07:16:38 +0900

    scaffolding(BE): url 수정

--- src/server/mock_data.py
+++ src/server/mock_data.py
@@ -7,9 +7,7 @@ DB = {
             "bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical "
             "revenge.\n\nLicensed under the Creative Commons Attribution "
             "license\nhttp://www.bigbuckbunny.org",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
             "subtitle": "By Blender Foundation",
             "thumb": "images/BigBuckBunny.jpg",
             "title": "Big Buck Bunny",
@@ -19,9 +17,7 @@ DB = {
         {
             "id": 1,
             "description": "The first Blender Open Movie from 2006",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
             "subtitle": "By Blender Foundation",
             "thumb": "images/ElephantsDream.jpg",
             "title": "Elephant Dream",
@@ -33,9 +29,7 @@ DB = {
             "description": "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For "
             "when you want to settle into your Iron Throne to watch the latest episodes. For "
             "$35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
             "subtitle": "By Google",
             "thumb": "images/ForBiggerBlazes.jpg",
             "title": "For Bigger Blazes",
@@ -47,9 +41,7 @@ DB = {
             "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for "
             "when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with "
             "Google Play Movies and more at google.com/chromecast.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
             "subtitle": "By Google",
             "thumb": "images/ForBiggerEscapes.jpg",
             "title": "For Bigger Escape",
@@ -60,9 +52,7 @@ DB = {
             "id": 4,
             "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For "
             "$35.  Find out more at google.com/chromecast.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
             "subtitle": "By Google",
             "thumb": "images/ForBiggerFun.jpg",
             "title": "For Bigger Fun",
@@ -74,9 +64,7 @@ DB = {
             "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for "
             "the times that call for bigger joyrides. For $35. Learn how to use Chromecast with "
             "YouTube and more at google.com/chromecast.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
             "subtitle": "By Google",
             "thumb": "images/ForBiggerJoyrides.jpg",
             "title": "For Bigger Joyrides",
@@ -88,9 +76,7 @@ DB = {
             "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for "
             "when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use "
             "Chromecast with Netflix and more at google.com/chromecast.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
             "subtitle": "By Google",
             "thumb": "images/ForBiggerMeltdowns.jpg",
             "title": "For Bigger Meltdowns",
@@ -107,9 +93,7 @@ DB = {
             "Amsterdam Blender Institute, by an international team of artists and developers. In "
             "addition to that, several crucial technical and creative targets have been realized "
             "online, by developers and artists and teams all over the world.\nwww.sintel.org",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
             "subtitle": "By Blender Foundation",
             "thumb": "images/Sintel.jpg",
             "title": "Sintel",
@@ -121,9 +105,7 @@ DB = {
             "description": "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes "
             "our customer-appreciation Balloon Launch will get some free T-shirts into the hands of "
             "our viewers.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
             "subtitle": "By Garage419",
             "thumb": "images/SubaruOutbackOnStreetAndDirt.jpg",
             "title": "Subaru Outback On Street And Dirt",
@@ -139,9 +121,7 @@ DB = {
             "Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out "
             "more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender "
             "Foundation - http://www.tearsofsteel.org",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
             "subtitle": "By Blender Foundation",
             "thumb": "images/TearsOfSteel.jpg",
             "title": "Tears of Steel",
@@ -153,9 +133,7 @@ DB = {
             "description": "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most "
             "requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's "
             "standard-setting lap time? Watch and see...",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
             "subtitle": "By Garage419",
             "thumb": "images/VolkswagenGTIReview.jpg",
             "title": "Volkswagen GTI Review",
@@ -167,9 +145,7 @@ DB = {
             "description": "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, "
             "and posting a video from the road every single day! The only place to watch them is by "
             "subscribing to The Smoking Tire or watching at BlackMagicShine.com",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
             "subtitle": "By Garage419",
             "thumb": "images/WeAreGoingOnBullrun.jpg",
             "title": "We Are Going On Bullrun",
@@ -181,9 +157,7 @@ DB = {
             "description": "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far "
             "$1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from "
             "CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
-            "url": [
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
-            ],
+            "url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
             "subtitle": "By Garage419",
             "thumb": "images/WhatCarCanYouGetForAGrand.jpg",
             "title": "What care can you get for a grand?",
