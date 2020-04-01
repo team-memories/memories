9484490f3364075347d49a0bdca4893722fe7541
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-02 07:38:07 +0900

    scaffolding(BE): 미구현 기능에 대한 에러 추가

--- src/server/resolvers/video.py
+++ src/server/resolvers/video.py
@@ -1,3 +1,13 @@
 from ariadne import ObjectType
 
 video = ObjectType("Video")
+
+
+@video.field("author")
+def resolve_author(_, __):
+    raise NotImplementedError("아직 Video type 의 author field 는  구현되지 않았습니다.")
+
+
+@video.field("comments")
+def resolve_author(_, __):
+    raise NotImplementedError("아직 Video type 의 comments field 는 구현되지 않았습니다.")
