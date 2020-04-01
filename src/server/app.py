f74e8730faa9c70b31752c0a36113d72815c1791
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-02 05:43:18 +0900

    scaffolding(BE): DB를 context로 전달

--- src/server/app.py
+++ src/server/app.py
@@ -1,6 +1,8 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+
+from mock_data import DB
 from resolvers.query import query
 
 TYPE_DEFS = load_schema_from_path("schema.graphql")
@@ -12,21 +14,34 @@ APP = Flask(__name__)
 
 @APP.route("/graphql", methods=["GET"])
 def graphql_playgroud():
-    # On GET request serve GraphQL Playground
-    # You don't need to provide Playground if you don't want to
-    # but keep on mind this will not prohibit clients from
-    # exploring your API using desktop GraphQL Playground app.
+    """
+    On GET request serve GraphQL Playground
+    You don't need to provide Playground if you don't want to
+    but keep on mind this will not prohibit clients from
+    exploring your API using desktop GraphQL Playground app.
+    """
+
     return PLAYGROUND_HTML, 200
+
+
+def context_function(request):
+    """
+    """
+    return {"request": request, "db": DB}
 
 
 @APP.route("/graphql", methods=["POST"])
 def graphql_server():
+    from flask import request
+
     # GraphQL queries are always sent as POST
     data = request.get_json()
 
     # Note: Passing the request to the context is optional.
     # In Flask, the current request is always accessible as flask.request
-    success, result = graphql_sync(SCHEMA, data, context_value=request, debug=APP.debug)
+    success, result = graphql_sync(
+        SCHEMA, data, context_value=context_function, debug=APP.debug
+    )
 
     status_code = 200 if success else 400
     return jsonify(result), status_code
