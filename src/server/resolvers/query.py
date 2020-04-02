f54a65f4282a82692294df5ff99127343e4506fb
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-03 00:35:38 +0900

    feat(delete-media): deleteMedia mutation 구현

--- src/server/resolvers/query.py
+++ src/server/resolvers/query.py
@@ -5,21 +5,23 @@ query = QueryType()
 
 
 @query.field("media")
-def resolve_media(_, info, _id):
-    videos = DB["videos"]
-
-    return videos[int(_id)]
+def resolve_media(_, __, _id):
+    videos: list = list(filter(lambda v: v["id"] == int(_id), DB["videos"]))
+    if len(videos) == 0:
+        raise ValueError("해당 id를 가진 미디어가 존재하지 않습니다.")
+    video = videos[0]
+    return video
 
 
 @convert_kwargs_to_snake_case
 @query.field("search")
 def resolve_search(
-    _,
-    info,
-    title: str = "",
-    location: str = "대한민국",
-    date_from="1900-01-01",
-    date_to="2099-12-31",
+        _,
+        info,
+        title: str = "",
+        location: str = "대한민국",
+        date_from="1900-01-01",
+        date_to="2099-12-31",
 ):
     videos = DB["videos"]
     videos = filter(lambda video: video["title"].find(title) != -1, videos)
