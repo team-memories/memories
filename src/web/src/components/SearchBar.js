2e65136f6e2f707bf32d3dd6cf5f20398f6d4cf8
gee05053 <gee05053@gmail.com>
2020-04-03 17:40:30 +0900

    Select 대신 DatePicker 사용

--- src/web/src/components/SearchBar.js
+++ src/web/src/components/SearchBar.js
@@ -17,8 +17,7 @@ export default class SearchBar extends Component {
     }
     this.onChangeTitle = this.onChangeTitle.bind(this)
     this.onChangeLocation = this.onChangeLocation.bind(this)
-    this.onChangeDateFrom = this.onChangeDateFrom.bind(this)
-    this.onChangeDateTo = this.onChangeDateTo.bind(this)
+    this.onChangeDate = this.onChangeDate.bind(this)
   }
   onChangeTitle = (e) => {
     this.setState({
@@ -30,14 +29,10 @@ export default class SearchBar extends Component {
       location : value[0] + " "  + value[1] + " " + value[2]
     })
   }
-  onChangeDateFrom (value) {
+  onChangeDate (value) {
     this.setState({
-      dateFrom : value
-    })
-  }
-  onChangeDateTo (value) {
-    this.setState({
-      dateTo : value
+      dateFrom : value[0]._d.getFullYear(),
+      dateTo : value[1]._d.getFullYear()
     })
   }
   render() {
@@ -46,8 +41,7 @@ export default class SearchBar extends Component {
         <Row gutter={[1,1]}>
           <SearchBarInput onChangeTitle={this.onChangeTitle}/>
           <SearchBarPlaceSelect onChangeLocation={this.onChangeLocation}/>
-          <SearchBarYearSelect text={"년 부터"} onChangeDate={this.onChangeDateFrom}/>
-          <SearchBarYearSelect text={"년 까지"} onChangeDate={this.onChangeDateTo}/>
+          <SearchBarYearSelect onChangeDate={this.onChangeDate}/>
           <SearchBarSubmitButton title={this.state.title} url={this.state.url} location={this.state.location} dateFrom={this.state.dateFrom} dateTo={this.state.dateTo}/>
         </Row>
       </Col>
