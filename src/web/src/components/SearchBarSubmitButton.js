d1a36f29ec1849a0072a065427549f11cd0e181e
gee05053 <gee05053@kookmin.ac.kr>
2020-04-03 21:50:44 +0900

    Update SearchBarSubmitButton.js
    todo 추가

--- src/web/src/components/SearchBarSubmitButton.js
+++ src/web/src/components/SearchBarSubmitButton.js
@@ -8,6 +8,7 @@ export default class SearchBarSubmitButton extends Component {
     return (
       <div>
         <Link to={url + this.props.title + "/" + this.props.location + "/" + this.props.dateFrom + "/" + this.props.dateTo}>
+      //todo(gee05053) : slash대신 query parameter 사용
           <Button>
               Search
           </Button>
