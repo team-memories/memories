b4ed33e75cf0758bcc8c5b7cdc10d28a8c0c478b
Sujin-Kim <sujinsu98@gmail.com>
2020-04-03 21:05:34 +0900

    branch 생성 - 필요한 파일 업로드

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ApolloProvider} from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client';
+import UploadPage from "./pages/UploadPage";
+import 'antd/dist/antd.css';
+import {InMemoryCache} from "apollo-cache-inmemory";
+import {createUploadLink} from "apollo-upload-client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const client = new ApolloClient({
+        link: createUploadLink({uri: 'http://localhost:9696/graphql'}),
+        cache: new InMemoryCache(),
+    });
+
+    return (
+        <div style={{paddingTop: '69px', minHeight: 'calc(100vh - 80px)'}}>
+            <ApolloProvider client={client}>
+                <UploadPage/>
+            </ApolloProvider>
+        </div>
+
+    )
 }
 
 export default App;
