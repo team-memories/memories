e9aa6fadd5329a27e93bf61172b2eb1c98a4a36a
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-04-04 11:43:23 +0900

    media detail query 구현

--- src/web/src/components/media-detail-delete-button.js
+++ src/web/src/components/media-detail-delete-button.js
@@ -1,28 +1,38 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import {useMutation} from '@apollo/react-hooks';
 import {Button, Popconfirm, message} from 'antd';
 
-// TODO(Lhyejin): Add useMutation for media delete
-
-// TODO(Lhyejin): Implement Delete Event Handler
-// 'Yes' Event Handler When 'Yes' is clicked in MediaDetailDeleteButton
-function Confirm(e) {
+// 서버에 삭제를 요청할 graphql mutation문
+const DELETE_MEDIA = gql`
+  mutation ($mediaId: ID!){
+    deleteMedia(_id: $mediaId){
+        _id
+    }
+  }
+`
+// TODO(Lhyejin): Implement Delete Event Handler, mediaId 가져오기
+// 'Yes'를 클릭했을 때, 이벤트 핸들러
+function Confirm(mediaId) {
+  console.log(mediaId)
+  // const [deleteMedia] = useMutation(DELETE_MEDIA);
   message.success('Delete Success')
 }
 
-// 'No' Event Handler When 'No' is clicked in MediaDetailDeleteButton
+// No를 클릭했을 때, 이벤트 핸들러
 function Cancel(e) {
   message.error('Delete Cancel')
 }
 
 /**
  * Button for Delete media
- * @param mediaId : Media Id to delete
+ * @param props.mediaId : 지울 미디어의 Id
  */
-function MediaDetailDeleteButton(mediaId) {
+function MediaDetailDeleteButton(props) {
   return (
       <Popconfirm
         title="미디어를 삭제하시겠습니까?"
-        onConfirm={Confirm}
+        onConfirm={() => Confirm(props.mediaId)}
         onCancel={Cancel}
         okText={"Yes"}
         cancelText={"No"}
--- src/web/src/pages/media-detail-page.js
+++ src/web/src/pages/media-detail-page.js
@@ -1,29 +1,28 @@
 import React, {Component} from 'react';
-import MediaDetailCard from '../components/media-detail-card'
+import MediaDetailCardQuery from '../components/media-detail-card-query'
+import MediaDetailComment from '../components/media-detail-comment'
+import {Row, Col} from 'antd'
 
+// TODO(Lhyejin): mediaId를 넘겨줄 때, MediaDetailCardQuery에
+// listpage에서 보내준 mediaId로 전달하기
+
+// Media Detail Page
 class MediaDetailPage extends Component {
   constructor(props) {
     super(props);
   }
 
+  // MediaDetailPage를 보여줌.
   render() {
-    const fakeData = {
-      id : 0,
-      title: 'title',
-      location: '충북 청주시',
-      date: '1998년',
-      user : {
-        profileImgUrl: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
-      },
-      url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-    };
     return(
-      <div>
-        <MediaDetailCard media={fakeData}/>
-      </div>
+      <Row>
+        <Col span={18} style={{padding: '3rem 4rem'}}>
+          {/*media를 보여주고, media에 대한 설명을 보여줌.*/}
+          <MediaDetailCardQuery mediaId={1} />
+        </Col>
+      </Row>
     );
   }
-
 }
 
 export default MediaDetailPage;
