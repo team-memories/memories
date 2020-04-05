fdcc68f47273e6031c9a78f511721f1fccefab79
gee05053 <gee05053@gmail.com>
2020-04-05 18:10:13 +0900

    query parameter 적용

--- src/web/src/components/SearchBarSubmitButton.js
+++ src/web/src/components/SearchBarSubmitButton.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react'
 import { Button } from 'antd'
 import { Link } from 'react-router-dom'
 
-const url = '/search/'
 export default class SearchBarSubmitButton extends Component {
   render() {
     return (
       <div>
-        <Link to={url + this.props.title + "/" + this.props.location + "/" + this.props.dateFrom + "/" + this.props.dateTo}>
-      //TODO(gee05053) : slash대신 query parameter 사용
+        <Link to={`/search/${this.props.title}/${this.props.location}/${this.props.dateFrom}/${this.props.dateTo}`}>
           <Button>
               Search
           </Button>
