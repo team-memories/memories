bedbbbae4e945557781e8d73d79b3d1b22a92b74
gee05053 <gee05053@gmail.com>
2020-04-06 04:03:03 +0900

    query parameter 적용

--- src/web/src/components/SearchBarSubmitButton.js
+++ src/web/src/components/SearchBarSubmitButton.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { Button } from 'antd'
 import { Link } from 'react-router-dom'
 
-export default class SearchBarSubmitButton extends Component {
+export default class SearchBarSubmitButton extends Component{
   render() {
     return (
       <div>
-        <Link to={`/search/${this.props.title}/${this.props.location}/${this.props.dateFrom}/${this.props.dateTo}`}>
+        <Link to={`/search?title=${this.props.title}&location=${this.props.location}&dateFrom=${this.props.dateFrom}&dateTo=${this.props.dateTo}`}>
           <Button>
               Search
           </Button>
