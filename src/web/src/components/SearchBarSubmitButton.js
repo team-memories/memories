2ea3cddbe860e5250fafd1cde80c7296b210fb77
gee05053 <gee05053@gmail.com>
2020-04-06 04:31:03 +0900

    pull request 지적 해결

--- src/web/src/components/SearchBarSubmitButton.js
+++ src/web/src/components/SearchBarSubmitButton.js
@@ -5,13 +5,11 @@ import { Link } from 'react-router-dom'
 export default class SearchBarSubmitButton extends Component{
   render() {
     return (
-      <div>
-        <Link to={`/search?title=${this.props.title}&location=${this.props.location}&dateFrom=${this.props.dateFrom}&dateTo=${this.props.dateTo}`}>
-          <Button>
-              Search
-          </Button>
-        </Link>
-      </div>
+      <Link to={`/search?title=${this.props.title}&location=${this.props.location}&dateFrom=${this.props.dateFrom}&dateTo=${this.props.dateTo}`}>
+        <Button>
+            Search
+        </Button>
+      </Link>
     )
   }
 }
