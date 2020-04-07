a864be82c91b4931e8de842add5287e1662fe6b3
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-04-07 14:05:37 +0900

    feat: media detail page로 routing 될 때, mediaId 정보받기 구현

--- src/web/src/pages/media-detail-page.js
+++ src/web/src/pages/media-detail-page.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import MediaDetailCardQuery from '../components/media-detail-card-query'
-import MediaDetailComment from '../components/media-detail-comment'
+import {withRouter} from 'react-router-dom';
 import {Row, Col} from 'antd'
-
-// TODO(Lhyejin): mediaId를 넘겨줄 때, MediaDetailCardQuery에
-// listpage에서 보내준 mediaId로 전달하기
 
 // Media Detail Page
 class MediaDetailPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      mediaId: '1',
+    }
+  }
+
+  // TODO(Lhyejin): mediaId 받을 때, listPage에서 보내준 방식으로 바꾸기
+  componentWillMount() {
+    const res_mediaId = this.props.location.search;
+    if (res_mediaId){
+      this.setState({
+        mediaId: res_mediaId
+      });
+    }
   }
 
   // MediaDetailPage를 보여줌.
@@ -18,11 +28,11 @@ class MediaDetailPage extends Component {
       <Row>
         <Col span={18} style={{padding: '3rem 4rem'}}>
           {/*media를 보여주고, media에 대한 설명을 보여줌.*/}
-          <MediaDetailCardQuery mediaId={1} />
+          <MediaDetailCardQuery mediaId={this.state.mediaId} />
         </Col>
       </Row>
     );
   }
 }
 
-export default MediaDetailPage;
+export default withRouter(MediaDetailPage);
