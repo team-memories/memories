ce906f962fb14932d82a3e069980cf40b41ab6eb
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-04-07 14:03:47 +0900

    fix: media를 불러올 때의 error handling 수정

--- src/web/src/components/media-detail-card-query.js
+++ src/web/src/components/media-detail-card-query.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import {useQuery} from '@apollo/react-hooks'
+import {useQuery} from '@apollo/react-hooks';
+import {useHistory} from 'react-router-dom';
 import MediaDetailCard from './media-detail-card';
+
 
 // media의 정보를 가져오는 graphql 쿼리
 const GET_MEDIA = gql`
@@ -25,11 +27,13 @@ const GET_MEDIA = gql`
 // 받아온 media 정보를 MediaDetailCard에 props로 전달함.
 // @param props.mediaId : 서버에 요청할 미디어의 Id
 function MediaDetailCardQuery(props) {
+  const history = useHistory();
   const {loading, error, data} = useQuery(GET_MEDIA, {
     variables: {mediaId:props.mediaId},
   });
   if (loading) return 'loading';
-  if (error) return `Error! ${error}`;
+  // error가 난다면 전 페이지로 redirect
+  if (error) return history.goBack();
   return (
     <MediaDetailCard media={data.media}/>
   );
