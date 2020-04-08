f25c9f0c88b531d60e93fca90d8d96fab2e156cd
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-04-08 15:52:52 +0900

    fix: lifecycle 삭제
    media id 받는 것 코멘트 처리하고 TODO 변경
    media-detail-page 이름 media-view-page로 변경

--- src/web/src/pages/media-view-page.js
+++ src/web/src/pages/media-view-page.js
@@ -4,35 +4,25 @@ import {withRouter} from 'react-router-dom';
 import {Row, Col} from 'antd'
 
 // Media Detail Page
-class MediaDetailPage extends Component {
+class MediaViewPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      mediaId: '1',
-    }
-  }
-
-  // TODO(Lhyejin): mediaId 받을 때, listPage에서 보내준 방식으로 바꾸기
-  componentWillMount() {
-    const res_mediaId = this.props.location.search;
-    if (res_mediaId){
-      this.setState({
-        mediaId: res_mediaId
-      });
-    }
   }
 
   // MediaDetailPage를 보여줌.
   render() {
+    // TODO(Lhyejin): list page에서 mediaId 보내는 것이 구현되면 comment 제거
+    // const res_mediaId = this.props.location.search;
+    const res_mediaId = 1;
     return(
       <Row>
         <Col span={18} style={{padding: '3rem 4rem'}}>
           {/*media를 보여주고, media에 대한 설명을 보여줌.*/}
-          <MediaDetailCardQuery mediaId={this.state.mediaId} />
+          <MediaDetailCardQuery mediaId={res_mediaId} />
         </Col>
       </Row>
     );
   }
 }
 
-export default withRouter(MediaDetailPage);
+export default withRouter(MediaViewPage);
