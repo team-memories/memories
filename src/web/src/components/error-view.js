006691494e3776ef4309927bc11934a57079617c
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-04-08 22:38:57 +0900

    feat: error component 구현

---
+++ src/web/src/components/error-view.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Result, Button} from 'antd';
+import {useHistory} from 'react-router-dom'
+
+// Error Component
+function ErrorView(){
+  const history = useHistory();
+  return(
+    <Result
+      title="Error"
+      subTitle="새로고침을 하시거나 홈으로 돌아가세요"
+      extra={[
+        <Button type="primary" key="home"
+          onClick={() => history.push('/')}
+        >
+          Home
+        </Button>
+      ]}
+    >
+    </Result>
+  )
+}
+
+export default ErrorView
