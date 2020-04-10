ec60d61721fa09eaca173991c2a0ad31d51aa232
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-10 16:04:42 +0900

    fix: Update package.json

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import {ApolloProvider} from '@apollo/react-hooks'
-import { ApolloClient } from 'apollo-client';
-import UploadPage from "./pages/UploadPage";
+import {ApolloProvider} from '@apollo/react-hooks';
+import {ApolloClient} from 'apollo-client';
+import UploadPage from './pages/UploadPage';
 import 'antd/dist/antd.css';
-import {InMemoryCache} from "apollo-cache-inmemory";
-import {createUploadLink} from "apollo-upload-client";
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {createUploadLink} from 'apollo-upload-client';
+
+const URI = 'http://localhost:9696/graphql';
 
 function App() {
-    const apolloCache = new InMemoryCache();
+  const client = new ApolloClient({
+    link: createUploadLink({URI}),
+    cache: new InMemoryCache(),
+  });
 
-    const uploadLink = createUploadLink({
-        uri: 'http://localhost:9696/graphql',
-    });
+  return (
+      <div style={{paddingTop: '69px', minHeight: 'calc(100vh - 80px)'}}>
+        <ApolloProvider client={client}>
+          <UploadPage/>
+        </ApolloProvider>
+      </div>
 
-    const client = new ApolloClient({
-        link: createUploadLink({uri: 'http://localhost:9696/graphql'}),
-        cache: new InMemoryCache(),
-    });
-
-    return (
-        <div style={{paddingTop: '69px', minHeight: 'calc(100vh - 80px)'}}>
-            <ApolloProvider client={client}>
-                <UploadPage/>
-            </ApolloProvider>
-        </div>
-
-    )
+  );
 }
 
 export default App;
