6d95ec88e1fc27d8ed37f6a59983cd496f4d2787
Sujin-Kim <sujinsu98@gmail.com>
2020-04-11 17:48:33 +0900

    layout center로 변경

--- src/web/src/pages/media-view-page.js
+++ src/web/src/pages/media-view-page.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import MediaDetailCardQuery from '../components/media-detail-card-query'
-import { useLocation } from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
 import {withRouter} from 'react-router-dom';
 import {Row, Col} from 'antd'
 
 function useQueryParm() {
-  return new URLSearchParams(useLocation().search)
+    return new URLSearchParams(useLocation().search)
 }
 
-function MediaViewPage(props) {
-  let query = useQueryParm()
-  return(
-    <Row>
-      <Col span={18} style={{padding: '3rem 4rem'}}>
-        <MediaDetailCardQuery mediaId={query.get("id")} />
-      </Col>
-    </Row>
-  );
+function MediaViewPage() {
+    let query = useQueryParm();
+    return (
+        <Row justify="center" style={{paddingTop: '3rem'}}>
+            <Col span={18}>
+                <MediaDetailCardQuery mediaId={query.get("id")}/>
+            </Col>
+        </Row>
+    );
 }
 
 export default withRouter(MediaViewPage);
