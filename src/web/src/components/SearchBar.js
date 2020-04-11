557f06bb77a107ff3e59422b15bb6e3510776c94
gee05053 <gee05053@gmail.com>
2020-04-12 01:24:08 +0900

    엔터 누르면 검색 가능

--- src/web/src/components/SearchBar.js
+++ src/web/src/components/SearchBar.js
@@ -1,55 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Row, Col } from 'antd'
 import SearchBarInput from './SearchBarInput'
 import SearchBarPlaceSelect from './SearchBarPlaceSelect'
 import SearchBarSubmitButton from './SearchBarSubmitButton'
 import SearchBarYearSelect from './SearchBarYearSelect'
+import { useHistory } from 'react-router-dom'
 
-export default class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title : '',
-      dateFrom : '',
-      dateTo : '',
-      location : ''
-    }
-    this.onChangeTitle = this.onChangeTitle.bind(this)
-    this.onChangeLocation = this.onChangeLocation.bind(this)
-    this.onChangeDate = this.onChangeDate.bind(this)
+function SearchBar() {
+  const [title, setTitle] = useState("")
+  const [location, setLocation] = useState("")
+  const [dateFrom, setDateFrom] = useState("")
+  const [dateTo, setDateTo] = useState("")
+  const history = useHistory()
+  const  onChangeTitle = (e) => {
+    setTitle(e.target.value)
   }
-  onChangeTitle = (e) => {
-    this.setState({
-      title : e.target.value
-    })
+  const onChangeLocation = (value) => {
+    setLocation(value[0] + " "  + value[1] + " " + value[2])
   }
-  onChangeLocation (value) {
-    this.setState({
-      location : value[0] + " "  + value[1] + " " + value[2]
-    })
+  const onChangeDate = (value) => {
+    setDateFrom(value[0]._d.getFullYear())
+    setDateTo(value[1]._d.getFullYear())
   }
-  onChangeDate (value) {
-    this.setState({
-      dateFrom : value[0]._d.getFullYear(),
-      dateTo : value[1]._d.getFullYear()
-    })
+  const onPressEnter = () => {
+    history.push(`/search?title=${title}&location=${location}&dateFrom=${dateFrom}&dateTo=${dateTo}`)
   }
-  render() {
-    return (
-      <Row>
-        <Col span={10}>
-          <SearchBarInput onChangeTitle={this.onChangeTitle}/>
-        </Col>
-        <Col span={3}>
-        < SearchBarPlaceSelect onChangeLocation={this.onChangeLocation}/>
-        </Col>
-        <Col span={4}>
-          <SearchBarYearSelect onChangeDate={this.onChangeDate}/>
-        </Col>
-        <Col span={1}>
-          <SearchBarSubmitButton title={this.state.title} location={this.state.location} dateFrom={this.state.dateFrom} dateTo={this.state.dateTo}/>
-        </Col>
-      </Row>
-    )
-  }
+  return (
+    <Row>
+      <Col span={10}>
+        <SearchBarInput onChangeTitle={onChangeTitle} onPressEnter={onPressEnter}/>
+      </Col>
+      <Col span={3}>
+      < SearchBarPlaceSelect onChangeLocation={onChangeLocation}/>
+      </Col>
+      <Col span={4}>
+        <SearchBarYearSelect onChangeDate={onChangeDate}/>
+      </Col>
+      <Col span={1}>
+        <SearchBarSubmitButton title={title} location={location} dateFrom={dateFrom} dateTo={dateTo}/>
+      </Col>
+    </Row>
+  )
 }
+export default SearchBar
