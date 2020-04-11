a868c5a6996fd1bb21ff849ee128912f4d009e92
gee05053 <gee05053@gmail.com>
2020-04-12 01:24:08 +0900

    엔터 누르면 검색 가능

--- src/web/src/components/SearchBarInput.js
+++ src/web/src/components/SearchBarInput.js
@@ -3,6 +3,13 @@ import { Input } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
 
 export default class SearchBarInput extends Component {
+  constructor(props) {
+    super(props)
+    this.onPressEnter = this.onPressEnter.bind(this)
+  }
+  onPressEnter () {
+    console.log("test")
+  }
   render() {
     return (
       <div>
@@ -10,6 +17,7 @@ export default class SearchBarInput extends Component {
           placeholder="Search"
           onChange={this.props.onChangeTitle}
           prefix={<SearchOutlined/>}
+          onPressEnter={this.props.onPressEnter}
         />
       </div>
     )
