eab8b62cefe965f942f9d2e695a888047680db20
gee05053 <gee05053@gmail.com>
2020-04-12 19:44:39 +0900

    Header 그림자 추가

--- src/web/src/components/Header.js
+++ src/web/src/components/Header.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
   render() {
     return (
       <Layout>
-        <Layout.Header style={{backgroundColor:'#F8F8F8', paddingInlineStart:"1.5%", minWidth: 1300, overflowX: 'auto'}}>
+        <Layout.Header style={{backgroundColor:'#F8F8F8', paddingInlineStart:"1.5%", minWidth: 1300, overflowX: 'auto', boxShadow: "5px 1px 7px #B8B8B8"}}>
           <Row align="middle" justify="center">
             <Col flex={1}>
               <HeaderLogo />
