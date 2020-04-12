e27b573412218de872481c7af046cba78780e1a8
SujinKim <sujinsu98@gmail.com>
2020-04-13 01:45:52 +0900

    indentation 수정

--- src/web/src/pages/media-view-page.js
+++ src/web/src/pages/media-view-page.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from 'react'
 import MediaDetailCardQuery from '../components/media-detail-card-query'
-import {useLocation} from 'react-router-dom'
-import {withRouter} from 'react-router-dom';
-import {Row, Col} from 'antd'
+import { useLocation } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import { Row, Col } from 'antd'
 
-function useQueryParm() {
-    return new URLSearchParams(useLocation().search)
+function useQueryParm () {
+  return new URLSearchParams(useLocation().search)
 }
 
-function MediaViewPage() {
-    let query = useQueryParm();
-    return (
-        <Row justify="center" style={{paddingTop: '3rem'}}>
-            <Col span={18}>
-                <MediaDetailCardQuery mediaId={query.get("id")}/>
-            </Col>
-        </Row>
-    );
+function MediaViewPage () {
+  let query = useQueryParm()
+  return (
+    <Row justify="center" style={{ paddingTop: '3rem' }}>
+      <Col span={18}>
+        <MediaDetailCardQuery mediaId={query.get('id')}/>
+      </Col>
+    </Row>
+  )
 }
 
-export default withRouter(MediaViewPage);
+export default withRouter(MediaViewPage)
