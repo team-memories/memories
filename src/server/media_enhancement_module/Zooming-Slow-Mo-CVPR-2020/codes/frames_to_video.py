600dddfc9d609255889c308511ea3a35ec80bebf
yun-kwak <rev1c0sm0s@gmail.com>
2020-04-18 14:13:57 +0000

    미디어 품질 모듈 baseline구현

--- src/server/media_enhancement_module/Zooming-Slow-Mo-CVPR-2020/codes/frames_to_video.py
+++ src/server/media_enhancement_module/Zooming-Slow-Mo-CVPR-2020/codes/frames_to_video.py
@@ -8,7 +8,6 @@ import cv2
 import torch
 
 import utils.util as util
-import data.util as data_util
 import models.modules.Sakuya_arch as Sakuya_arch
 
 import argparse
@@ -17,31 +16,11 @@ from shutil import rmtree
 # For parsing commandline arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--ffmpeg_dir", type=str, default="", help='path to ffmpeg.exe')
-parser.add_argument("--video", type=str, required=True, help='path of video to be converted')
 parser.add_argument("--model", type=str, required=True, help='path of pretrained model')
 parser.add_argument("--fps", type=float, default=24, help='specify fps of output video. Default: 24.')
 parser.add_argument("--N_out", type=int, default=7, help='Specify size of output frames of the network for faster conversion. This will depend on your cpu/gpu memory. Default: 7')
 parser.add_argument("--output", type=str, default="output.mp4", help='Specify output file name. Default: output.mp4')
 args = parser.parse_args()
-
-def check():
-    """
-    Checks the validity of commandline arguments.
-    Parameters
-    ----------
-        None
-    Returns
-    -------
-        error : string
-            Error message if error occurs otherwise blank string.
-    """
-
-    error = ""
-    if (args.batch_size not in [3, 5, 7]):
-        error = "Error: --N_out has to be 3 or 5 or 7"
-    # if ".mkv" not in args.output:
-        # error = "output needs to have a video container"
-    return error
 
 def main():
     scale = 4
@@ -54,15 +33,9 @@ def main():
     model = Sakuya_arch.LunaTokis(64, N_ot, 8, 5, 40)
 
     #### extract the input video to temporary folder
-    save_folder = osp.join(osp.dirname(args.output), '.delme')
-    save_out_folder = osp.join(osp.dirname(args.output), '.hr_delme')
-    util.mkdirs(save_folder)
+    save_folder = osp.join(osp.dirname(args.output), 'VFI_input')
+    save_out_folder = osp.join(osp.dirname(args.output), 'VFI_output')
     util.mkdirs(save_out_folder)
-    error = util.extract_frames(args.ffmpeg_dir, args.video, save_folder)
-    if error:
-        print(error)
-        exit(1)
-
     # temporal padding mode
     padding = 'replicate'
     save_imgs = True
