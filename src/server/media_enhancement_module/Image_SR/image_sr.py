5e00258b0ccd44d26e0f82f05ccd97fcffd4b85a
Lhyejin <hub0913@kookmin.ac.kr>
2020-04-21 15:29:13 +0000

    feat: update image_sr condition

--- src/server/media_enhancement_module/Image_SR/image_sr.py
+++ src/server/media_enhancement_module/Image_SR/image_sr.py
@@ -13,20 +13,27 @@ ARGS = parser.parse_args()
 test_dir = ARGS.test_dir
 result_path=ARGS.out_dir
 
+# image 불러오기 위해서 image path가져오기
 old_names = os.popen("ls %s"%test_dir).readlines()
 img_names = [None]*len(old_names)
 for idx in range(len(old_names)):
     img_names[idx] = test_dir+old_names[idx][:-1]
 
-#rdn = RDN(weights='psnr-large')
+rdn = RDN(weights='noise-cancel')
 rrdn = RRDN(weights='gans')
 for img_name in img_names:
     img = imread(img_name)
     lr_img = np.array(img)
-    sr_img = rrdn.predict(lr_img)
+
+    if lr_img.shape[0] >= 360 or lr_img.shape[1] >= 640:
+        sr_img = rdn.predict(lr_img)
+        print('noise')
+    else:
+        sr_img = rrdn.predict(lr_img)
+        print('gans')
+
     base = os.path.basename(img_name)
     img_name = os.path.splitext(base)[0]
-    print(img_name)
     if not os.path.exists(result_path):
         os.makedirs(result_path)
 
