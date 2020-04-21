455a18737243eaa0c85e7e145de8069147c24f1b
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-22 04:41:22 +0900

    fix

--- src/server/media_enhancement_module/convert_color_to_black_and_white.py
+++ src/server/media_enhancement_module/convert_color_to_black_and_white.py
@@ -1,4 +1,4 @@
-from PIL import Image 
+from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
@@ -6,6 +6,5 @@ path = 'data/color_input'
 
 files = [f for f in listdir(path) if isfile(join(path, f))]
 for file_name in files:
-    image = Image.open(join(path, file_name))
-    image.convert('L')
+    image = Image.open(join(path, file_name)).conver('LA')
     image.save(join('data/bnw_original_input', file_name))
