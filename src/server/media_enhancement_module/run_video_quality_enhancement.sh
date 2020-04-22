68e9adc99beea93db0821be1d183cf925f1211e6
Arin Kwak <rev1c0sm0s@gmail.com>
2020-04-23 05:45:34 +0900

    FPS 두 배 추가

--- src/server/media_enhancement_module/run_video_quality_enhancement.sh
+++ src/server/media_enhancement_module/run_video_quality_enhancement.sh
@@ -1,20 +1,25 @@
 # Pre-processing
 
 # get frame rate
-cp data/input.mp4 data/input_preprocessed.mp4 && \
+ffmpeg -i data/input.mp4 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > data/fps
+
 conda activate color && \
-ffmpeg -i "data/input_preprocessed.mp4" -vf scale=480:270 "data/input_preprocessed.mp4" && \
-ffmpeg -i "data/input_preprocessed.mp4" -vf hue=s=0 "data/input_preprocessed.mp4" && \
-ffmpeg -i "data/input_preprocessed.mp4" -vsync 0 "data/color_input/%06d.png" && \
+ffmpeg -i "data/input.mp4" -vf scale=480:270 "data/input_scaled.mp4" && \
+ffmpeg -i "data/input_scaled.mp4" -vf hue=s=0 "data/input_bw.mp4" && \
+ffmpeg -i "data/input_bw.mp4" -vsync 0 "data/color_input/%06d.png" && \
 
 # Input => Colorization => Zooming Slow-mo => Output
 # Colorization
+conda activate color && \
 python colorization/main_woflow_up.py --model colorization/ckpt_woflow \
---use_gpu 0 --test_dir data/color_input --out_dir data/SR_input/ && \
+--use_gpu 0 --test_dir data/color_input --out_dir data/VFI_input/ && \
+conda activate zooming-slow-mo && \
 cd Zooming-Slow-Mo-CVPR-2020/codes && \
+fps=$(cat ../../data/fps)
+fps=$(($fps*2))
 python frames_to_video.py --model ../experiments/pretrained_models/xiang2020zooming.pth \
     --input ../../data/VFI_input \
-    --output ../../data/output.mp4 --fps 60 --N_out 2 && \
+    --output ../../data/output.mp4 --fps $fps  --N_out 3 && \
 cd - && \
 
 # Clean up
