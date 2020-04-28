1788831ad66182889c17b4c473b9e392764e712d
W-C-G <55659912+W-C-G@users.noreply.github.com>
2020-04-28 23:33:27 +0900

    change from class to function

--- src/web/src/components/Media/media-card.js
+++ src/web/src/components/Media/media-card.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { Avatar, Card } from 'antd'
 
-export default class MediaCard extends Component {
-  render () {
-    return (
-      <Link to={`/watch?id=${this.props.id}`}>
-        <Card
-          style={{ height: '95%', borderRadius: 10 }}
-          bordered={true}
-          cover={<img style={{ width: '100%', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-                      src={this.props.url} alt="thumbnail"/>}
-        >
-          <Card.Meta
-            avatar={
-              <Avatar size={40} src={this.props.author.profileImgUrl} shape="circle"/>
-            }
-            description={
-              <div style={{
-                display: 'inline-block',
-                maxWidth: '100%',
-                textOverflow: 'ellipsis',
-                whiteSpace: 'nowrap',
-                overflow: 'hidden',
-                marinTop: 10,
-                marginBottom: 10
-              }}>
-              <span style={{
-                fontWeight: 'bold',
-                fontSize: '1.5em',
-                color: 'black',
-              }}>
-                            {this.props.title}
-              </span>
-                <br/>
-                {this.props.author.name}
-                <br/>
-                {this.props.year} / {this.props.location}
-              </div>
-            }
-          />
-        </Card>
-      </Link>
-    )
-  }
+function MediaCard(props){
+  const id = useState(props.id)
+  const url = useState([props.url])
+  const profileImgUrl = useState([props.author.profileImgUrl])
+  const title = useState([props.title])
+  const location = useState([props.location])
+  const year = useState(props.year)
+  const name = useState([props.author.name])
+  
+  return (
+    <Link to={`/watch?id=${id}`}>
+      <Card
+        style={{ height: '95%', borderRadius: 10 }}
+        bordered={true}
+        cover={<img style={{ width: '100%', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
+                    src={url} alt="thumbnail"/>}
+      >
+        <Card.Meta
+          avatar={
+            <Avatar size={40} src={profileImgUrl} shape="circle"/>
+          }
+          description={
+            <div style={{
+              display: 'inline-block',
+              maxWidth: '100%',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              marinTop: 10,
+              marginBottom: 10
+            }}>
+            <span style={{
+              fontWeight: 'bold',
+              fontSize: '1.5em',
+              color: 'black',
+            }}>
+              {title}
+            </span>
+              <br/>
+              {name}
+              <br/>
+              {year} / {location}
+            </div>
+          }
+        />
+      </Card>
+    </Link>
+  )
 }
+
+export default MediaCard
