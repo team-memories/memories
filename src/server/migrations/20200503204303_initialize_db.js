9c08b781da7eebb35b84c682b94cbf9dcf98f480
Arin Kwak <rev1c0sm0s@gmail.com>
2020-05-03 22:13:19 +0900

    scaffolding: password 추가

--- src/server/migrations/20200503204303_initialize_db.js
+++ src/server/migrations/20200503204303_initialize_db.js
@@ -2,6 +2,7 @@ exports.up = async function (knex) {
   await knex.schema.createTable("user", function (table) {
     table.increments("id").unsigned().primary();
     table.string("email").notNullable();
+    table.string("password").notNullable();
     table.string("name").notNullable();
     table.text("profile_img_url");
   });
