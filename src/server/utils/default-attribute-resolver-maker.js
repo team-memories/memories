f387f23789546d08919d0925767573f077ede1ea
Arin Kwak <rev1c0sm0s@gmail.com>
2020-05-04 00:17:10 +0900

    scaffolding: Update

--- src/server/utils/default-attribute-resolver-maker.js
+++ src/server/utils/default-attribute-resolver-maker.js
@@ -2,7 +2,7 @@ module.exports = (attrNames, dataSourceName) => {
   const resolvers = {};
   for (const attrName of attrNames) {
     resolvers[attrName] = async ({ id }, _, { dataSources }) => {
-      return dataSources[dataSourceName].getAttribute(id, attrName);
+      return dataSources[dataSourceName].getAttribute(attrName, id);
     };
   }
   return resolvers;
