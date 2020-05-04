2bd505ea92557c078a4975bfe4dc66f00c151e01
gee05053 <gee05053@gmail.com>
2020-05-05 00:37:36 +0900

    스타일 수정

--- src/web/src/pages/user-page.js
+++ src/web/src/pages/user-page.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function UserPage () {
   return(
-    <div>
-      user-page
+    <div style={{margin : '2rem'}}>
+      <h1>
+        내가 올린 동영상
+      </h1>
     </div>
   )
 }
