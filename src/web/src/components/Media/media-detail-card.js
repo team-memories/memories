4fade09c3700dfb7224fea679ebb4f98f566ad7a
SujinKim <sujinsu98@gmail.com>
2020-05-17 03:01:55 +0900

    더보기 기능 추가

--- src/web/src/components/Media/media-detail-card.js
+++ src/web/src/components/Media/media-detail-card.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import { Avatar, List, Typography, Divider } from 'antd';
+import { Avatar, List, Divider } from 'antd';
 import MediaDetailDeleteButton from './media-detail-delete-button';
 import ImageCompare from './media-detail-card-image-compare';
 import MediaAddComment from './media-add-comment';
-import MediaCommentQuery from './media-comment-query';
+import MediaCommentQuery from './MediaComment/media-comment-query';
 
 function MediaInfo (props) {
   return (
     <div>
-    <List>
-      <List.Item
-        actions={[
-          <div>
-            {
-            // 작성자와 로그인 정보가 같을 경우에만 deleteButton 보이도록 설정
-              (props.media.author.id === sessionStorage.getItem("user_id")) &&
+      <List>
+        <List.Item
+          actions={[
+            <div>
+              {
+                // 작성자와 로그인 정보가 같을 경우에만 deleteButton 보이도록 설정
+                (props.media.author.id === sessionStorage.getItem("user_id")) &&
               <MediaDetailDeleteButton mediaId={props.media.id}/>
-            }
-          </div>
-        ]}
-      >
-        <List.Item.Meta
-          avatar={<Avatar size={50} src={props.media.author.profileImgUrl}/>}
-          title={<div style={{fontSize: 20}}>{props.media.title}</div>}
-          description={<div style={{fontSize: 15}}>{props.media.author.name}</div>}
-          style={{ textAlign: 'left' }}
-        />
-        <div style={{fontSize: 15}}>{props.media.location}<br/>{props.media.year}</div>
-      </List.Item>
-      <List.Item>
-        {props.media.description}
-      </List.Item>
-    </List>
+              }
+            </div>
+          ]}
+        >
+          <List.Item.Meta
+            avatar={<Avatar size={50} src={props.media.author.profileImgUrl}/>}
+            title={<div style={{fontSize: 20}}>{props.media.title}</div>}
+            description={<div style={{fontSize: 15}}>{props.media.author.name}</div>}
+            style={{ textAlign: 'left' }}
+          />
+          <div style={{fontSize: 15}}>{props.media.location}<br/>{props.media.year}</div>
+        </List.Item>
+        <List.Item>
+          {props.media.description}
+        </List.Item>
+      </List>
       <Divider/>
     </div>
   );
