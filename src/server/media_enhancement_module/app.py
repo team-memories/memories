9a0d26dc95b786c2f55a9495a4bc885b6f4f8abc
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-17 16:35:51 +0900

    feat(upload): 각 모듈간의 PATH 전달을 명확하게 수정

--- src/server/media_enhancement_module/app.py
+++ src/server/media_enhancement_module/app.py
@@ -1,4 +1,4 @@
-import subprocess
+import os
 
 from flask import Flask, request, jsonify
 import requests
@@ -7,67 +7,82 @@ app = Flask(__name__)
 MEDIA_DATA_PATH = "/media_data/"
 
 
-def preprocess_video(path):
+def preprocess_video(file_in_path, folder_out_path):
+    url = 'http://localhost:4201/v1/preprocess'
+    requests.post(url, json={'file_in_path': file_in_path, 'folder_out_path': folder_out_path})
+
+
+def video_colorization(folder_in_path, folder_out_path):
     url = 'http://localhost:4202/v1/enhance'
-    requests.post(url, json={'path': path})
-    return True
+    requests.post(url, json={'folder_in_path': folder_in_path, 'folder_out_path': folder_out_path})
 
 
-
-def video_colorization(path):
-    url = 'http://localhost:4202/v1/enhance'
-    requests.post(url, json={'path': path})
-    return True
-
-
-def super_resolution_and_video_interpolation(path):
+def super_resolution_and_video_interpolation(folder_in_path, folder_out_path):
     url = 'http://localhost:4203/v1/enhance'
-    requests.post(url, json={'path': path})
-    return True
+    requests.post(url, json={'folder_in_path': folder_in_path, 'folder_out_path': folder_out_path})
 
 
 @app.route('/v1/enhance/video', methods=['POST'])
-def convert_video():
+def enhance_video():
     param = request.get_json(force=True)
-    path, id = param["path"], param["id"]
-    preprocess_video(path)
-    video_colorization(path)
-    super_resolution_and_video_interpolation(path)
+    file_name, id = param["file_name"], param["id"]
+    file_path = os.path.join(MEDIA_DATA_PATH, file_name)
+    enhanced_media_folder_path = file_path + "_enhanced"
+    os.system(f"mkdir -p {enhanced_media_folder_path}")
+
+    file_in_path = file_path
+    frames_out_folder_path = os.path.join(enhanced_media_folder_path, "frames")
+    preprocess_video(file_in_path, frames_out_folder_path)
+
+    folder_in_path = frames_out_folder_path
+    folder_out_path = os.path.join(enhanced_media_folder_path, "color")
+    video_colorization(folder_in_path, folder_out_path)
+
+    folder_in_path = frames_out_folder_path
+    folder_out_path = os.path.join(enhanced_media_folder_path, "color_sr")
+    super_resolution_and_video_interpolation(folder_in_path, folder_out_path)
 
 
-def image_preprocess(path):
-    os.system(f"mkdir -p {path}/color_input/")
-    file_name = os.path.basename(path).replace("_enhanced", "")
-    os.system(f"cp {path} {path}/color_input/{file_name}")
+def image_preprocess(file_in_path, file_out_path):
+    pass
 
 
-def image_colorization(path):
-    return video_colorization(path)
+def image_colorization(file_in_path, file_out_path):
+    file_name = os.path.basename(file_in_path)
+    os.system(f"mkdir -p {file_in_path}_enhanced/color_input/")
+    os.system(f"cp {file_in_path} {file_in_path}_enhanced/color_input/{file_name}")
+    folder_in_path = file_in_path + "_enhanced/color_input/"
+    folder_out_path = file_in_path + "_enhanced/color_output/"
+    video_colorization(folder_in_path, folder_out_path)
+    os.system(f"cp {os.path.join(folder_out_path, file_name)} {file_out_path}")
 
 
-def image_super_resolution(path):
-    url = 'http://localhost:4103/v1/convert'
-    requests.post(url, json={'path': path})
-    return True
+def image_super_resolution(file_in_path, file_out_path):
+    url = 'http://localhost:4103/v1/enhance'
+    requests.post(url, json={'file_in_path': file_in_path, 'file_out_path': file_out_path})
 
 
 @app.route("/v1/enhance/photo", methods=['POST'])
-def convert_photo():
+def enhance_photo():
     param = request.get_json(force=True)
-    path, id = param["path"], param["id"]
-    file_name = os.path.basename(path)
-    enhanced_media_path = MEDIA_DATA_PATH + file_name + "_enhanced"
-    os.system(f"mkdir -p {enhanced_media_path}")
+    file_name, id = param["file_name"], param["id"]
+    file_path = os.path.join(MEDIA_DATA_PATH, file_name)
+    enhanced_media_folder_path = file_path + "_enhanced"
+    os.system(f"mkdir -p {enhanced_media_folder_path}")
 
-    image_preprocess(enhanced_media_path)  # port=4101
-    image_colorization(enhanced_media_path)  # port=4202
-    image_super_resolution(enhanced_media_path)  # port=4103
+    # image_preprocess(None, None)
 
-    # TODO(yun-kwak): 에러 핸들링
+    file_in_path = file_path
+    file_out_path = os.path.join(enhanced_media_folder_path, "color_" + file_name)
+    image_colorization(file_in_path, file_out_path)
 
-    # S3에 업로드
+    file_in_path = file_out_path
+    file_out_path = os.path.join(enhanced_media_folder_path, "color_sr_" + file_name)
+    image_super_resolution(file_in_path, file_out_path)
 
-    # 변환 완료시 DB에 업데이
+    # file_path와 file_out_path를 AWS S3에 업로드
+
+    # 변환 완료시 DB에 업데이트
 
 
 app.run(debug=True, port=4001)
