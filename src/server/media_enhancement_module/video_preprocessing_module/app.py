7cbf1af5c498fe9e9cc295ca612cb5bab1a2203e
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-17 17:39:31 +0900

    feat(upload): video preprocess module 완성

--- src/server/media_enhancement_module/video_preprocessing_module/app.py
+++ src/server/media_enhancement_module/video_preprocessing_module/app.py
@@ -1,32 +1,34 @@
 import subprocess
-import os
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-def extract_thumbnail(video_in_path, thumbnail_out_path):
+def extract_thumbnail(file_in_path, thumbnail_out_path):
+    subprocess.run(['ffmpeg', '-i', file_in_path, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_out_path])
 
-    subprocess.run(['ffmpeg', '-i', video_in_path, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_out_path])
 
-def preprocess(path):
-    extract_thumbnail(path)
+def preprocess(file_in_path, frames_folder_out_path, thumbnail_out_path, fps_out_path):
+    subprocess.run(f"mkdir -p {frames_folder_out_path}", shell=True)
+    extract_thumbnail(file_in_path, thumbnail_out_path)
     subprocess.run(f"""
-    ffmpeg -i {path} 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > {path}.fps
-    source activate color && \
-    ffmpeg -y -i "{path}" -vf scale=480:270 "{path}_input_scaled.mp4" && \
-    ffmpeg -y -i "{path}_input_scaled.mp4" -vf hue=s=0 "{path}_input_bw.mp4"  && \
-    mkdir {path}_color_input && \
-    ffmpeg -y -i "{path}_input_bw.mp4" -vsync 0 "{path}_color_input/%06d.png"  && \
-    source deactivate
+    ffmpeg -i {file_in_path} 2>&1 | grep -o '[0-9]\\{{1,3\\}}\\sfps'| sed 's/\\sfps//' > {fps_out_path} && \
+    ffmpeg -y -i "{file_in_path}" -vf scale=480:270 "{file_in_path}_enhanced/input_scaled.mp4" && \
+    ffmpeg -y -i "{file_in_path}_enhanced/input_scaled.mp4" -vf hue=s=0 "{file_in_path}_enhanced/input_bw.mp4"  && \
+    ffmpeg -y -i "{file_in_path}_enhanced/input_bw.mp4" -vsync 0 "{frames_folder_out_path}/%06d.png"
     """, shell=True)
+
 
 @app.route("/v1/preprocess", methods=['POST'])
 def enhance():
     param = request.get_json(force=True)
-    video_in_path,  = param["video_in_path"], param["video_out_path"]
-    preprocess(path)
+    file_in_path, frames_folder_out_path, thumbnail_out_path, fps_out_path = \
+        param["file_in_path"], param["frames_folder_out_path"], param["thumbnail_out_path"], param["fps_out_path"]
+    preprocess(file_in_path, frames_folder_out_path, thumbnail_out_path, fps_out_path)
+    return {
+        "success": True
+    }
 
 
-app.run(debug=True, port=4201)
+app.run(debug=True, port=4201, host="0.0.0.0")
