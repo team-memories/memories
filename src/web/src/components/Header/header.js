ec58b343fcfdba66d9d2d2ad4b84474ee33d5655
W-C-G <55659912+W-C-G@users.noreply.github.com>
2020-05-18 22:50:37 +0900

    modify location of searchbar

--- src/web/src/components/Header/header.js
+++ src/web/src/components/Header/header.js
@@ -10,10 +10,10 @@ function Header () {
       <Col style={{marginTop: '20px', marginBottom: '20px', marginLeft: '30px'}}xl={{marginleft: '50px'}}>
         <HeaderLogo/>
       </Col>
-      <Col offset={2} xs={{span: 5, offset: 1}} sm={{span:7.5}} md={{span: 10}} xl={{span: 15}} style={{marginTop: '10px', marginBottom:'10px'}}>
+      <Col offset={2} xs={{span: 5, offset: 1}} sm={{span:7.5}} md={{span: 10}} xl={{span: 15}} xxl={{offset: 2, span: 14}} style={{marginTop: '10px', marginBottom:'10px'}}>
         <SearchBar/>
       </Col>
-      <Col flex={1.5}>
+      <Col flex={1.5} xxl={{offset: 2}}>
         <HeaderIcons/>
       </Col>
     </Row>
