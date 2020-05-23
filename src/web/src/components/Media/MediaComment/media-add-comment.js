07951bfe4602ae4b7b5366de5836b75b4cb42592
SujinKim <sujinsu98@gmail.com>
2020-05-23 16:14:12 +0900

    로그인 안되었을 때 댓글 작성 못하도록 수정

--- src/web/src/components/Media/MediaComment/media-add-comment.js
+++ src/web/src/components/Media/MediaComment/media-add-comment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 import { Comment, Avatar, Form, Button, Input, message, Space } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 const { TextArea } = Input;
 
@@ -20,7 +21,7 @@ const CREATE_COMMENT = gql`
     }
 `;
 
-const Editor = ({ onChange, onCancel, onSubmit, submitting, value }) => (
+const UserEditor = ({ onChange, onCancel, onSubmit, submitting, value }) => (
   <div>
     {/* 댓글 입력창 */}
     <Form.Item>
@@ -50,6 +51,36 @@ const Editor = ({ onChange, onCancel, onSubmit, submitting, value }) => (
     </Space>
   </div>
 );
+
+const GuestEditor = ({onChange, onCancel, value}) => (
+  <div>
+    {/* 댓글 입력창 */}
+    <Form.Item>
+      <TextArea
+        rows={1}
+        onChange={onChange}
+        value={value}
+        autoSize={{ minRows: 1, maxRows: 6 }}
+        placeholder="로그인이 필요한 서비스입니다."
+        style={{border: "none", borderBottom: "1px solid"}} />
+    </Form.Item>
+    <Space style={{float: 'right'}}>
+      {/* 댓글 취소 버튼 */}
+      <Form.Item>
+        <Button onClick={onCancel} style={{border: 'none'}}>
+          취소
+        </Button>
+      </Form.Item>
+
+      {/* submit 버튼 */}
+      <Form.Item>
+        <Button htmlType="submit" onClick={() => {message.error('로그인이 필요한 서비스입니다.');}} type="primary">
+          댓글
+        </Button>
+      </Form.Item>
+    </Space>
+  </div>
+); 
 
 // parent: MediaComment
 function MediaAddComment (props) {
@@ -95,28 +126,51 @@ function MediaAddComment (props) {
     setCommentValue(e.target.value);
   };
 
-  return (
-    <div>
-      <div style={{fontWeight: 'bold', fontSize: '18px'}}>댓글</div>
-      <Comment
-        // 사용자 섬네일 이미지 설정 
-        avatar={
-          <Avatar
-            src={sessionStorage.getItem("user_profileImgUrl")}
-          />
-        }
-        // 댓글 입력
-        content={
-          <Editor
-            onChange={handleChange}  // 댓글을 입력할 때마다 input 에 update 한다.
-            onCancel={handleCancel}  // 작성중인 댓글의 내용을 지운다.
-            onSubmit={handleSubmit}  // submit mutation 수행
-            value={commentValue} // 작성한 댓글의 value
-          />
-        }
-      />
-    </div>
-  );
+  // 로그인이 되어 있는 경우 
+  if (sessionStorage.getItem("token")) {
+    return (
+      <div>
+        <div style={{fontWeight: 'bold', fontSize: '18px'}}>댓글</div>
+        <Comment
+          // 사용자 섬네일 이미지 설정
+          avatar={
+            <Avatar
+              src={sessionStorage.getItem("user_profileImgUrl")}
+            />
+          }
+          // 댓글 입력
+          content={
+            <UserEditor
+              onChange={handleChange}  // 댓글을 입력할 때마다 input 에 update 한다.
+              onCancel={handleCancel}  // 작성중인 댓글의 내용을 지운다.
+              onSubmit={handleSubmit}  // submit mutation 수행
+              value={commentValue} // 작성한 댓글의 value
+            />
+          }
+        />
+      </div>
+    );
+  }
+  // 로그인이 되어 있지 않은 경우
+  else {
+    return (
+      <div>
+        <div style={{fontWeight: 'bold', fontSize: '18px'}}>댓글</div>
+        <Comment
+          // 게스트 섬네일 이미지 설정
+          avatar={<Avatar icon={<UserOutlined/>} style={{backgroundColor:"#82BEE6"}}/>}
+          // 댓글 입력
+          content={
+            <GuestEditor
+              onChange={handleChange}
+              onCancel={handleCancel}
+              value={commentValue}
+            />
+          }
+        />
+      </div>
+    );
+  }
 }
 
 export default MediaAddComment;
