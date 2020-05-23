670de9d3eb44e567ececd8751efd2eb8d2a2a658
Lhyejin <hub0913@kookmin.ac.kr>
2020-05-23 12:05:07 +0000

    feat: argument에 gpu 추가

--- src/server/media_enhancement_module/image_colorization/image_colorize.py
+++ src/server/media_enhancement_module/image_colorization/image_colorize.py
@@ -1,25 +1,27 @@
 from deoldify import device
 from deoldify.device_id import DeviceId
-device.set(device=DeviceId.GPU0)
-
 import torch
-import fastai
-from deoldify.visualize import *
-from PIL import Image
-import os
-import argparse
+import argparse, os
+#choices:  CPU, GPU0...GPU7
+torch.backends.cudnn.benchmark=True
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--in_dir", default="data/color_input", type=str, help="input folder")
 parser.add_argument("--out_dir", default="data/output", type=str, help="output folder")
-parser.add_argument("--render_factor", default=20, type=int, help="render factor")
+parser.add_argument("--gpu", default=DeviceId.CPU, type=DeviceId.argparse, choices=list(DeviceId))
 
 args = parser.parse_args()
 in_dir = args.in_dir
 out_dir = args.out_dir
-render_factor= args.render_factor
-
+gpu = args.gpu
 os.makedirs(out_dir, exist_ok=True)
+
+device.set(device=gpu)
+
+import fastai
+from deoldify.visualize import *
+from PIL import Image
+
 
 image_names = [f for f in os.listdir(in_dir) if os.path.isfile(os.path.join(in_dir, f))]
 
