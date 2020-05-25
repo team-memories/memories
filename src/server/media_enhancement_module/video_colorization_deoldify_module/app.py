36fdbe32288e39de8aa32d1394835fc9299307eb
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-26 04:42:41 +0900

    feat(upload): Deoldify video colorization 컨테이너화

--- src/server/media_enhancement_module/video_colorization_deoldify_module/app.py
+++ src/server/media_enhancement_module/video_colorization_deoldify_module/app.py
@@ -1,97 +1,24 @@
-# import the necessary packages
-import os
-import sys
-import requests
-import ssl
-from flask import Flask
-from flask import request
-from flask import jsonify
-from flask import send_file
+import subprocess
 
-
-from app_utils import download
-from app_utils import generate_random_filename
-from app_utils import clean_me
-from app_utils import clean_all
-from app_utils import create_directory
-from app_utils import get_model_bin
-from app_utils import convertToJPG
-
-from os import path
-import torch
-
-import fastai
-from deoldify.visualize import *
-from pathlib import Path
-import traceback
-
-
-torch.backends.cudnn.benchmark=True
-
-
-os.environ['CUDA_VISIBLE_DEVICES']='0'
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
+def process(folder_in_path, folder_out_path):
+    subprocess.run(f"python video_colorize.py --in_dir {folder_in_path} \
+            --out_dir {folder_out_path} --gpu 0")
 
-# define a predict function as an endpoint
-@app.route("/process", methods=["POST"])
-def process_image():
 
-    input_path = generate_random_filename(upload_directory,"jpeg")
-    output_path = os.path.join(results_img_directory, os.path.basename(input_path))
-
+@app.route("/v1/enhance", methods=['POST'])
+def enhance():
+    param = request.get_json(force=True)
+    folder_in_path, folder_out_path = param["folder_in_path"], param["folder_out_path"]
     try:
-        url = request.json["source_url"]
-        render_factor = int(request.json["render_factor"])
-
-        download(url, input_path)
-
-        try:
-            image_colorizer.plot_transformed_image(path=input_path, figsize=(20,20),
-                render_factor=render_factor, display_render_factor=True, compare=False)
-        except:
-            convertToJPG(input_path)
-            image_colorizer.plot_transformed_image(path=input_path, figsize=(20,20),
-            render_factor=render_factor, display_render_factor=True, compare=False)
-
-        callback = send_file(output_path, mimetype='image/jpeg')
-        
-        return callback, 200
-
+        process(folder_in_path, folder_out_path)
     except:
-        traceback.print_exc()
-        return {'message': 'input error'}, 400
-
-    finally:
-        pass
-        clean_all([
-            input_path,
-            output_path
-            ])
-
-if __name__ == '__main__':
-    global upload_directory
-    global results_img_directory
-    global image_colorizer
-
-    upload_directory = '/data/upload/'
-    create_directory(upload_directory)
-
-    results_img_directory = '/data/result_images/'
-    create_directory(results_img_directory)
-
-    model_directory = '/data/models/'
-    create_directory(model_directory)
-    
-    artistic_model_url = 'https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth?dl=0'
-    get_model_bin(artistic_model_url, os.path.join(model_directory, 'ColorizeArtistic_gen.pth'))
+        return {}, 500
+    return {}, 200
 
 
-    image_colorizer = get_image_colorizer(artistic=True)
-    
-    port = 5000
-    host = '0.0.0.0'
-
-    app.run(host=host, port=port, threaded=False)
+app.run(debug=True, port=4202, host="0.0.0.0")
