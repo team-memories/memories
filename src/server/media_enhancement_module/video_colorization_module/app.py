6ed21a84767ad14afc30725d92c772b30316a3b3
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-26 23:39:49 +0900

    feat(upload): 기존 video colorization module 삭제, deoldify로 변경

--- src/server/media_enhancement_module/video_colorization_module/app.py
+++ src/server/media_enhancement_module/video_colorization_module/app.py
@@ -1,21 +1,23 @@
 import subprocess
 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-def video_colorization(folder_in_path, folder_out_path):
-    subprocess.run(f"python colorization/main_woflow_up.py --model colorization/ckpt_woflow \
-    --use_gpu 0 --test_dir {folder_in_path} --out_dir {folder_out_path}", shell=True)
+def process(file_in_path, folder_out_path, bw_frame_out_path):
+    subprocess.run(f"python video_colorize.py --in_file {file_in_path} \
+            --out_dir {folder_out_path} in_frame_dir {bw_frame_out_path} --gpu gpu0", shell=True)
 
 
 @app.route("/v1/enhance", methods=['POST'])
 def enhance():
     param = request.get_json(force=True)
-    folder_in_path, folder_out_path = param["folder_in_path"], param["folder_out_path"]
-    video_colorization(folder_in_path, folder_out_path)
+    print(param)
+    file_in_path, folder_out_path, bw_frame_out_path = param["file_in_path"], param["folder_out_path"], param["bw_frame_out_path"]
+    process(file_in_path, folder_out_path, bw_frame_out_path)
     return {}, 200
 
 
-app.run(debug=True, port=4202, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(port=4202, host="0.0.0.0")
