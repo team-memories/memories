bb2899dc63c29adea815e2d9e4ea7cbe006b4de9
SujinKim <sujinsu98@gmail.com>
2020-05-27 16:30:37 +0900

    delete button 크기 줄임

--- src/web/src/components/UserPage/user-media-deleteButton.js
+++ src/web/src/components/UserPage/user-media-deleteButton.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import gql from 'graphql-tag'
-import { useMutation } from '@apollo/react-hooks'
-import { Button, message, Popconfirm } from 'antd'
-import { DeleteOutlined } from '@ant-design/icons'
+import React from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { Button, message, Popconfirm } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 
 const DELETE_MEDIA = gql`
   mutation deleteMedia($mediaId: ID!){
@@ -13,24 +13,24 @@ const DELETE_MEDIA = gql`
       }
     }
   }
-`
+`;
 
 function UserMediaDeleteButton (props) {
   const [deleteMedia] = useMutation(DELETE_MEDIA, {
     onCompleted ({ deleteMedia: { title, author } }) {
-      message.success(`${author.name}의 ${title}가(이) 삭제되었습니다.`)
-      window.location.reload()
+      message.success(`${author.name}의 ${title}가(이) 삭제되었습니다.`);
+      window.location.reload();
     },
     onError () {
-      message.error('삭제를 요청한 미디어가 존재하지 않습니다.')
+      message.error('삭제를 요청한 미디어가 존재하지 않습니다.');
     }
-  })
+  });
   const confirm = (mediaId) => {
-    deleteMedia({ variables: { mediaId } })
-  }
-  const cancel = (e) => {
-    message.error('삭제를 취소하였습니다.')
-  }
+    deleteMedia({ variables: { mediaId } });
+  };
+  const cancel = () => {
+    message.error('삭제를 취소하였습니다.');
+  };
   return (
     <Popconfirm
       title="미디어를 삭제하시겠습니까?"
@@ -39,9 +39,9 @@ function UserMediaDeleteButton (props) {
       okText={'Yes'}
       cancelText={'No'}
     >
-      <Button type="link" icon={<DeleteOutlined style={{fontSize: '25px'}}/>} style={{float: "right"}} />
+      <Button type="link" icon={<DeleteOutlined style={{fontSize: '17px'}}/>} style={{float: "right"}} />
     </Popconfirm>
-  )
+  );
 }
 
-export default UserMediaDeleteButton
+export default UserMediaDeleteButton;
