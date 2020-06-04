f7023205788008c17156fb8c5c5124fdd302a86d
Memories <hub0913@kookmin.ac.kr>
2020-06-04 14:57:01 +0000

    feat: video 조건분기 테스트 완료

--- src/server/media_enhancement_module/video_preprocessing_module/app.py
+++ src/server/media_enhancement_module/video_preprocessing_module/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def check_color(file_in_path):
     MONOCHROMATIC_MAX_VARIANCE = 0.005
     COLOR = 1000
-    MAYBE_COLOR = 100
+    MAYBE_COLOR = 400
     is_color = True
     v = ImageStat.Stat(Image.open(file_in_path)).var
     is_monochromatic = reduce(lambda x, y: x and y < MONOCHROMATIC_MAX_VARIANCE, v, True)
@@ -25,10 +25,19 @@ def check_color(file_in_path):
     return is_color
 
 
-def check_SR(file_in_path):
+def check_image_SR(file_in_path):
     image = Image.open(file_in_path)
     is_sr = True
     if (image.size[0]>=720 and image.size[1]>=1280) or (image.size[0]>=1280 and image.size[1]>=720):
+        is_sr = False
+
+    return is_sr
+
+
+def check_video_SR(file_in_path):
+    image = Image.open(file_in_path)
+    is_sr = True
+    if (image.size[0]>=480 and image.size[1]>=720) or (image.size[0]>=720 and image.size[1]>=480):
         is_sr = False
 
     return is_sr
@@ -46,30 +55,31 @@ def check_file_size(file_in_path):
 
 
 def video_to_frame(file_in_path, frames_folder_out_path):
-    subprocess.run(["ffmpeg", "-i", file_in_path, os.path.join(frames_folder_out_path, "%05d.png")])
+    os.makedirs(frames_folder_out_path, exist_ok=True)
+    subprocess.run(["ffmpeg", "-i", file_in_path, os.path.join(frames_folder_out_path, "%05d.png"), "-y"])
 
 
 def frame_to_video(folder_in_path, file_out_path, fps_in_path):
     fps = open(fps_in_path).readline()
-    subprocess.run(['ffmpeg', '-r', fps, '-f', 'image2', '-i', os.path.join(folder_in_path,'%05d.jpg'),
-                     '-vcodec', 'libx264', '-acodec', 'aac', fps_in_path, '-y'])
+    subprocess.run(['ffmpeg', '-r', fps, '-f', 'image2', '-i', os.path.join(folder_in_path,'%05d.png'),
+                     '-vcodec', 'libx264', '-acodec', 'aac', file_out_path, '-y'])
 
 
 def video_preprocess(file_in_path, thumbnail_out_path, fps_out_path):
-    subprocess.run(['ffmpeg', '-i', file_in_path, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_out_path])
+    subprocess.run(['ffmpeg', '-i', file_in_path, '-ss', '00:00:00.000', '-vframes', '1', thumbnail_out_path, '-y'])
     subprocess.run(f"""ffmpeg -i {file_in_path} 2>&1 | sed -n "s/.*, \\(.*\\) fp.*/\\1/p" > {fps_out_path}""",
                    shell=True)
 
     size_error = check_file_size(file_in_path)
     is_color = check_color(thumbnail_out_path)
-    is_sr = check_SR(thumbnail_out_path)
+    is_sr = check_video_SR(thumbnail_out_path)
 
     return size_error, is_color, is_sr
 
 
 def image_preprocess(file_in_path):
     is_color = check_color(file_in_path)
-    is_sr = check_SR(file_in_path)
+    is_sr = check_image_SR(file_in_path)
     return is_color, is_sr
 
 
