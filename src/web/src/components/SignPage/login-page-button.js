fb66c1cfad32c448896172d9bf7a644eaa8ff65e
SujinKim <sujinsu98@gmail.com>
2020-06-06 15:05:13 +0900

    Log in -> Login

--- src/web/src/components/SignPage/login-page-button.js
+++ src/web/src/components/SignPage/login-page-button.js
@@ -7,7 +7,7 @@ function LoginPageButton (props) {
     //Log in 버튼과 Register가 있음, Log in버튼을 누르면 로그인 진행, Register를 누르면 회원가입 화면 이동 
     <Form.Item>
       <Button htmlType="submit" onClick={props.onClickLogin} style={{marginInlineEnd: '70%'}}>
-        Log in
+        Login
       </Button>
       <Link to="/register">
         Register
