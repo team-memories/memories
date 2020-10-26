9f8391886c0d0af1ebabf33b25fb5d5e69e1b288
SujinKim <sujinsu98@gmail.com>
2020-10-26 10:07:19 +0900

    feat(server): add await

--- src/server/migrations/20201009173516_rename_body_to_content.js
+++ src/server/migrations/20201009173516_rename_body_to_content.js
@@ -1,11 +1,11 @@
 exports.up = async function (knex) {
-  knex.schema.table("comment", function (table) {
+  await knex.schema.table("comment", function (table) {
     table.renameColumn("body", "content");
   });
 };
 
 exports.down = async function (knex) {
-  knex.schema.table("comment", function (table) {
+  await knex.schema.table("comment", function (table) {
     table.renameColumn("content", "body");
   });
 };
