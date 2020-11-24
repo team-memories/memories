45a0f58016542118abd64979e5ba4a090bcc46f6
SujinKim <sujinsu98@gmail.com>
2020-11-24 23:01:14 +0900

    refactor(frontend): add badge to video in user media card

--- src/web/src/components/UserPage/user-media-card.js
+++ src/web/src/components/UserPage/user-media-card.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Avatar, Card } from 'antd';
+import { Avatar, Badge, Card } from 'antd';
 import UserMediaDeleteButton from './user-media-deleteButton';
 import { ColorArray } from '../constants';
+import { PlayCircleFilled } from '@ant-design/icons';
 
 function MediaCard(props){
   return (
@@ -10,9 +11,16 @@ function MediaCard(props){
       style={{ height: '100%', borderRadius: 10 }}
       bordered={true}
       //유저 이미지 카드에서 카드 전체를 눌러서 detail-view-page로 이동하게 만들면 삭제기능 불가능, 여기서는 이미지만 누를 수 있도록 설정
-      cover={(props.typename === 'Photo') ? <Link to={{pathname: `/watch`, search: `?id=${props.id}`, state: {data: props.data}}}><img style={{ width: '100%', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-        src={props.url} alt="thumbnail"/></Link> : <Link to={{pathname: `/watch`, search: `?id=${props.id}`, state: {data: props.data}}}><img style={{ width: '100%', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-        src={props.thumbnailUrl} alt="thumbnail"/></Link> }
+      cover={(props.typename === 'Photo') ?
+        <Link to={{pathname: `/watch`, search: `?id=${props.id}`, state: {data: props.data}}}>
+          <img style={{ width: '100%', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src={props.url} alt="thumbnail"/>
+        </Link>
+        :
+        <Badge size="default" count={<PlayCircleFilled/>} style={{color: '#f5222d'}} offset={[-20, 20]}>
+          <Link to={{pathname: `/watch`, search: `?id=${props.id}`, state: {data: props.data}}}>
+            <img style={{ width: '100%', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src={props.thumbnailUrl} alt="thumbnail"/>
+          </Link>
+        </Badge>}
     >
       <Card.Meta
         avatar={(props.author.profileImgUrl === '') ?
