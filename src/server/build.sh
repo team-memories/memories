5ad760744adaf2f97e823b86882e4e2d68b65f60
yunhyeok-kwak <yunhyeok.kwak@navercorp.com>
2020-11-24 20:12:52 +0900

    feat(MEM): recover MEM containers

--- src/server/build.sh
+++ src/server/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 TAG=b0.2 && \
-docker build . -t main_api_server:${TAG}
+docker build . -t main_api_server:${TAG} && \
+cd media_enhancement_module && \
+source build.sh && \
+cd -
